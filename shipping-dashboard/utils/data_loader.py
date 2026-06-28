94f48b281872957ea9e363176c2f4775152924dc
Ayman khan <aymnakhan.aiml22@gmail.com>
2026-06-28 15:16:45 +0530

    .

--- shipping-dashboard/utils/data_loader.py
+++ shipping-dashboard/utils/data_loader.py
@@ -1,10 +1,13 @@
-# utils/data_loader.py
-
 import pandas as pd
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATA_PATH = BASE_DIR / "data" / "cleaned_shipping_data.csv"
 
 def load_data():
-    df = pd.read_csv("data/cleaned_shipping_data.csv")
-
+    df = pd.read_csv(DATA_PATH)
     df['Order Date'] = pd.to_datetime(df['Order Date'])
     df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
