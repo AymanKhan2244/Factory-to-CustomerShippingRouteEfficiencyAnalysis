89d38bc1b7f8e9d8c101b24b9439b8b0cb718d4f
Ayman khan <aymnakhan.aiml22@gmail.com>
2026-06-28 16:32:06 +0530

    ..

--- shipping-dashboard/utils/data_loader.py
+++ shipping-dashboard/utils/data_loader.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATA_PATH = BASE_DIR / "data" / "cleaned_shipping_data.csv"
+DATA_PATH = BASE_DIR / "data" / "Cleaned_shipping_data.csv"
 
 def load_data():
     df = pd.read_csv(DATA_PATH)
